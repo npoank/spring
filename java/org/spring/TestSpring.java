5a29121d77bc254eef95eaf1e9005dc1d7e5d120
Kulbicki Andrey <kubl-ml@mail.ru>
2022-10-18 12:00:21 +0300

    scope singleton

--- java/org/spring/TestSpring.java
+++ java/org/spring/TestSpring.java
@@ -9,8 +9,20 @@ public class TestSpring {
         );
 
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstmusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstmusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstmusicPlayer.setVolume(10);
+
+        System.out.println(firstmusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
